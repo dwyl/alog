530f7e04da93d233b9fa5573da6f5aab61fa9278
SimonLab <simon.labondance@gmail.com>
2019-02-04 12:57:39 +0000

    update tests, #30

--- test/all_test.exs
+++ test/all_test.exs
@@ -14,10 +14,18 @@ defmodule AlogTest.AllTest do
     test "does not include old items" do
       {:ok, user} = %User{} |> User.changeset(Helpers.user_1_params()) |> User.insert()
       {:ok, _} = %User{} |> User.changeset(Helpers.user_2_params()) |> User.insert()
-      {:ok, user_updated} = user |> User.changeset(%{postcode: "W2 3EC"}) |> User.update()
+      {:ok, _} = user |> User.changeset(%{postcode: "W2 3EC"}) |> User.update()
 
       assert length(User.all()) == 2
-      assert user.inserted_at == user_updated.inserted_at
+    end
+
+    test "all return inserted_at original value" do
+      {:ok, user} = %User{} |> User.changeset(Helpers.user_3_params()) |> User.insert()
+      {:ok, user_updated} = user |> User.changeset(%{postcode: "W2 3EC"}) |> User.update()
+
+      [user_all] = User.all()
+      assert user_all.inserted_at == user.inserted_at
+      assert user_all.postcode == user_updated.postcode
     end
   end
 end
