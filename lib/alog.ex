94808f3794047cb851b74078186e003fe148b056
Robert Francis <robertefrancis18@gmail.com>
2019-02-28 13:13:28 +0000

    adds remaining defdelgate to required functions #45

--- lib/alog.ex
+++ lib/alog.ex
@@ -3,16 +3,24 @@ defmodule Alog do
     driver: :postgrex,
     migration_lock: "FOR UPDATE"
 
-  @impl true
-  def supports_ddl_transaction? do
-    true
-  end
+  alias Ecto.Adapters.Postgres, as: EAP
 
   @behaviour Ecto.Adapter.Storage
 
+  # Why did we define our own version of this function?
+  # Sorry if I have missed something that has been explained already.
   @impl true
-  defdelegate storage_up(opts), to: Ecto.Adapters.Postgres
+  def supports_ddl_transaction?, do: true
 
   @impl true
-  defdelegate storage_down(opts), to: Ecto.Adapters.Postgres
+  defdelegate storage_up(opts), to: EAP
+
+  @impl true
+  defdelegate storage_down(opts), to: EAP
+
+  @impl true
+  defdelegate structure_dump(default, config), to: EAP
+
+  @impl true
+  defdelegate structure_load(default, config), to: EAP
 end
--- lib/alog/connection.ex
+++ lib/alog/connection.ex
@@ -1,16 +1,52 @@
 defmodule Alog.Connection do
+  alias Ecto.Adapters.Postgres.Connection, as: EAPC
+
   @behaviour Ecto.Adapters.SQL.Connection
+  @default_port 5432
 
   @impl true
-  defdelegate ddl_logs(result), to: Ecto.Adapter.Postgres
+  def child_spec(opts) do
+    opts
+    |> Keyword.put_new(:port, @default_port)
+    |> Postgrex.child_spec()
+  end
 
   @impl true
-  defdelegate prepare_execute(connection, name, statement, params, options),
-    to: Ecto.Adapter.Postgres
+  defdelegate ddl_logs(result), to: EAPC
 
   @impl true
-  defdelegate query(connection, statement, params, options), to: Ecto.Adapter.Postgres
+  defdelegate prepare_execute(conn, name, statement, params, opts), to: EAPC
 
   @impl true
-  defdelegate stream(connection, statement, params, options), to: Ecto.Adapter.Postgres
+  defdelegate query(conn, statement, params, opts), to: EAPC
+
+  @impl true
+  defdelegate stream(conn, statement, params, opts), to: EAPC
+
+  @impl true
+  defdelegate to_constraints(error_struct), to: EAPC
+
+  @impl true
+  defdelegate execute(conn, query, params, opts), to: EAPC
+
+  @impl true
+  defdelegate all(query), to: EAPC
+
+  @impl true
+  defdelegate update_all(query, prefix \\ nil), to: EAPC
+
+  @impl true
+  defdelegate delete_all(query), to: EAPC
+
+  @impl true
+  defdelegate insert(prefix, table, header, rows, on_conflict, returning), to: EAPC
+
+  @impl true
+  defdelegate update(prefix, table, fields, filters, returning), to: EAPC
+
+  @impl true
+  defdelegate delete(prefix, table, filters, returning), to: EAPC
+
+  @impl true
+  defdelegate execute_ddl(arg), to: EAPC
 end
