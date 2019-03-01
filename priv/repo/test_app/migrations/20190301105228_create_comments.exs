eaa87b510dd565783e044d75613df393970f436d
Robert Francis <robertefrancis18@gmail.com>
2019-03-01 15:04:10 +0000

    Adds timestamps to migration file. (leaves timestamps out of schema on purpose) #45

--- priv/repo/test_app/migrations/20190301105228_create_comments.exs
+++ priv/repo/test_app/migrations/20190301105228_create_comments.exs
@@ -4,10 +4,13 @@ defmodule Alog.Repo.Migrations.CreateComments do
   def change do
     create table(:comments, primary_key: false) do
       # cid & entry_id need to be removed later as they should be handled in execute_ddl I believe
+      # timestamps are needed in alog but may or may not be in the schema.
       add(:cid, :string, primary_key: true)
       add(:entry_id, :string)
       add(:comment, :string)
       add(:deleted, :boolean, default: false)
+
+      timestamps()
     end
   end
 end
